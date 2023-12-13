4e0b617bb5f7ee46d05ab0e3a95d951104964665
edbc <becerracenmanueld@gmail.com>
2023-12-12 22:50:46 -0400

    update

--- examples/Input.cpp
+++ examples/Input.cpp
@@ -1,30 +1,12 @@
 #include <node++/node++.h>
-#include <node++/simkey.h>
+#include <node++/input.h>
 #include <node++/timer.h>
 
 using namespace nodepp;
 
-#define VELOCITY .05
-
-ptr_t<float> vec ({ 0, 0 });
-
 void $Ready() { 
 
-    timer::interval([](){
-        control::mouse::set_position( vec[0], vec[1] );
-    },1);
-
-    timer::interval([](){
-
-        static float x = 0; x += VELOCITY;
-
-        vec[0] = 50 + sin( x * 1 ) * 20;
-        vec[1] = 50 + cos( x * 2 ) * 20;
-
-    },100);
-
-    timer::timeout([](){
-        process::exit(1);
-    },100000);
+    input_t inp ();
+    inp.set_mouse_position( 0, 0 );
 
 }
