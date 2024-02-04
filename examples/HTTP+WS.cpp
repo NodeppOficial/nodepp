e030a982192ed8f5bf351e3cc6d5b8a159a1e72b
edbc <becerracenmanueld@gmail.com>
2024-02-04 13:29:27 -0400

    update

---
+++ examples/HTTP+WS.cpp
@@ -0,0 +1,86 @@
+#include <nodepp/nodepp.h>
+#include <nodepp/cluster.h>
+#include <nodepp/timer.h>
+#include <nodepp/http.h>
+#include <nodepp/path.h>
+#include <nodepp/date.h>
+#include <nodepp/zlib.h>
+#include <nodepp/ws.h>
+#include <nodepp/fs.h>
+
+/*────────────────────────────────────────────────────────────────────────────*/
+
+using namespace nodepp;
+
+/*────────────────────────────────────────────────────────────────────────────*/
+
+void _main_() {
+
+    auto server = http::server([=]( http_t cli ){ 
+
+        string_t dir = "www/index.html";
+        if( cli.path.size() > 1 )
+            dir = path::join( "www", cli.path );
+
+        console::log( cli.path, cli.get_fd() );
+
+        if( !fs::exists_file(dir) ){
+            cli.write_headers( 404, {{ { "content-type", "text/plain" } }} );
+            cli.write( string::format("404: Oops time: %s",date::fulltime().data()) ); 
+            cli.close(); return;
+        }
+
+        auto str = fs::readable( dir );
+
+        if( cli.headers["Range"].empty() ){
+
+            cli.write_headers( 200, {{
+                { "Content-Length", string::to_string(str.size()) },
+              //{ "Cache-Control", "public, max-age=3600" },
+                { "Content-Type",   path::mimetype(dir) },
+                { "Content-Encoding", "deflate" }
+            }});
+
+            if(!regex::test(path::mimetype(dir),"audio|video","i") ) 
+                zlib::deflate::pipe( str, cli );
+
+        } elif ( !cli.headers["Range"].empty() ) {
+
+            array_t<string_t> range = regex::match_all(cli.headers["Range"],"\\d+","i");
+            ulong rang[2]; rang[0] = string::to_ulong( range[0] );
+                  rang[1] = min( rang[0]+CHUNK_MB(10), str.size()-1 );
+
+            cli.write_headers( 206, {{
+                { "Content-Range", string::format("bytes %lu-%lu/%lu",rang[0],rang[1],str.size()) },
+                { "Content-Type",  path::mimetype(dir) }, 
+                { "Accept-Range", "bytes" }
+            }});
+
+            str.set_range( rang[0], rang[1] );
+            stream::pipe( str, cli );
+
+        }
+
+    }); ws::server( server );
+
+    server.onConnect([]( ws_t cli ){
+
+        console::log("connected");
+
+        cli.onData([=]( string_t data ){
+            console::log( data );
+        });
+
+        cli.onClose([=](){
+            console::log("closed");
+        });
+
+    });
+
+    server.listen( "localhost", 8000, [=]( socket_t server ){
+        console::log("server started at http://localhost:8000");
+    });
+
+}
+
+/*────────────────────────────────────────────────────────────────────────────*/
--- include/nodepp/http.h
+++ include/nodepp/http.h
@@ -143,7 +143,7 @@ public:
         if( !is_available() )                              _End;
         base = read_line(); protocol = "HTTP";
         if( !regex::test( base,"HTTP/\\d\\.\\d" ) )        _End; 
-        init = regex::match_all( base, "[^\\s\t\r\n ]+" ); _Next;
+        init = regex::match_all( base, "[^\\s\t\r\n ]+" );
         
         if( !regex::test( init[1], "^\\d+" ) ) {
             auto idx = init[1].index_of([]( char x ){ return x=='?'; });
@@ -160,7 +160,7 @@ public:
             url     = string::format( "https://%s%s%s", (char*)headers["Host"], (char*)path, (char*)search );
         } else {
             version = init[0]; status = string::to_int(init[1]);
-        }   _Next;
+        }   //_Next;
 
         do {  line = read_line(); idx = line.index_of([]( char x ){ return x==':'; });
             if( idx < 0 ){ break; } a = line.slice( 0,idx ).to_capital_case();                        
