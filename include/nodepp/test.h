b18f08f6118d912e64a2cfbbf63fced09347f0f2
edbcrepo <becerracenmanueld@gmail.com>
2024-03-10 18:59:40 -0400

    update

--- include/nodepp/test.h
+++ include/nodepp/test.h
@@ -14,53 +14,165 @@
 
 /*────────────────────────────────────────────────────────────────────────────*/
 
-namespace nodepp { namespace test {
-
-    struct NODE {
-        bool                     enable;
-        queue_t<function_t<int>> queue;
-        string_t                 name;
-    };  queue_t<NODE> group;
-
-    template< class... T >
-    void push( const string_t& name, const function_t<int,T...>& func ) {
-        
-    }
-
-    void run() {
-
-    }
-
-}}
+#include "event.h"
 
 /*────────────────────────────────────────────────────────────────────────────*/
 
-#define TEST_UNIGNORE( NAME ) nodepp::test::set(0);
-    auto y = nodepp::test::group.get_index([]( NODE x ){ return x.name==NAME; }); \
-    if ( y>= 0 ){ nodepp::test::group[y]->data.enable = 0; }
+namespace nodepp {
 
-#define TEST_IGNORE( NAME ) nodepp::test::set(1);
-    auto y = nodepp::test::group.get_index([]( NODE x ){ return x.name==NAME; }); \
-    if ( y>= 0 ){ nodepp::test::group[y]->data.enable = 1; }
+    class test_t { 
+    protected:
 
-#define TEST_ADD(NAME) nodepp::test::add(NAME);
-    auto y = nodepp::test::group.get_index([]( NODE x ){ return x.name==NAME; }); \
-    if ( y < 0 ){ \
-        nodepp::test::NODE item; item.name = NAME; \
-        nodepp::test::group[y].push( NODE ); \
-    }
+        struct NODE {
+            function_t<int> callback;
+            string_t        name;
+        };  
+        
+        struct DONE {
+            queue_t<NODE> queue;
+            void*  ev = nullptr;
+            int state = 1; 
+        };  ptr_t<DONE> obj;
 
-#define TEST_SET(NAME,...) nodepp::test::push( NAME, __VA_ARGS__ );
+    public:
 
-#define TEST_ASSERT ( CONDITION ) if( !CONDITION ) TEST_FAIL();
+        event_t<> onClose;
+        event_t<> onFail;
+        event_t<> onDone;
+        event_t<> onSkip;
 
-#define TEST_END( NAME ) TEST_IGNORE( NAME )
+        /*-------------------------------------------------------------------*/
 
-#define TEST_RUN(NAME) nodepp::test::run();
+        ~test_t() noexcept {
+            if( obj.count()  > 1 ){ return; }
+            if( obj->state == -1 ){ return; }
+   	        process::onSIGERR.off( obj->ev );
+            onClose.emit(); obj->state =-1;
+        }
 
-#define TEST_FAIL() return -1;
+        test_t() noexcept : obj( new DONE() ) { 
+            auto self = type::bind( this );
+            obj->ev = process::onSIGERR.once([=]( ... ){ 
+                conio::error( "ERROR: " ); 
+                auto node = self->obj->queue.get(); 
+                conio::log( node->data.name );
+                conio::error( "FAILED\n\n" ); 
+            });
+        }
 
-#define TEST_DONE() return  1; 
+        /*-------------------------------------------------------------------*/
+
+        template< class T >
+        void set( const string_t& name, const T& callback ) noexcept {
+            NODE node; 
+                 node.callback = callback;
+                 node.name     = name;
+            obj->queue.push( node );
+        }
+
+        /*-------------------------------------------------------------------*/
+
+        void   ignore() const noexcept { obj->state =-1; }
+
+        void unignore() const noexcept { obj->state = 1; }
+        
+        /*-------------------------------------------------------------------*/
+
+        void await() const noexcept {
+
+            auto x = obj->queue.first(); int result = 0;
+
+            while( x != nullptr && obj->state != -1 ){
+                conio::done("\nTEST:> "); conio::log( x->data.name, "\n" );
+                result = x->data.callback(); if ( result == 1 ){
+                    conio::done( "DONE: " ); 
+                    conio::log( x->data.name );
+                    conio::done( "PASSED\n\n" ); 
+                    this->onDone.emit();
+                } elif ( result == -1 ) {
+                    conio::error( "ERROR: " ); 
+                    conio::log( x->data.name );
+                    conio::error( "FAILED\n\n" ); 
+                    this->onFail.emit();
+                } else {
+                    conio::warn( "WARNING: " );
+                    console::warning( x->data.name );
+                    conio::warn( "SKIPPED\n\n" ); 
+                    this->onSkip.emit();
+                }   
+            x = x->next; process::next(); }
+
+        }
+        
+        /*-------------------------------------------------------------------*/
+
+        void run() const noexcept {
+            auto self = type::bind( this );
+
+            process::add([=](){
+                if( self->obj->state != 1 ){ return -1; }
+                static auto x = self->obj->queue.first();
+                int result = 0;
+            coStart
+
+                while( x != nullptr ){
+                    conio::done("\nTEST:> "); conio::log( x->data.name, "\n" );
+                    result = x->data.callback(); if ( result == 1 ){
+                        conio::done( "DONE: " );
+                        console::done( x->data.name ); 
+                        conio::done( "PASSED\n\n" ); 
+                        self->onDone.emit();
+                    } elif ( result == -1 ) {
+                        conio::error( "ERROR: " );
+                        console::error( x->data.name );
+                        conio::error( "FAILED\n\n" ); 
+                        self->onFail.emit();
+                    } else {
+                        conio::warn( "WARNING: " );
+                        console::warning( x->data.name );
+                        conio::warn( "SKIPPED\n\n" ); 
+                        self->onSkip.emit();
+                    }   x = x->next; coNext;
+                }
+
+            coStop
+            });
+
+        }
+
+    };
+
+}
+
+/*────────────────────────────────────────────────────────────────────────────*/
+
+#define TEST_ADD( TEST, NAME, ... ) TEST.set( NAME, __VA_ARGS__ )
+
+#define TEST_ASSERT ( CONDITION ) if( !CONDITION ) TEST_FAIL()
+
+#define TEST_END( TEST ) TEST_IGNORE( TEST ); return 0
+
+/*---------------------------------------------------------------------------*/
+
+#define TEST_UNIGNORE( TEST ) TEST.unignore()
+
+#define TEST_IGNORE( TEST ) TEST.ignore()
+
+/*---------------------------------------------------------------------------*/
+
+#define TEST_AWAIT( TEST ) TEST.await()
+
+#define TEST_CREATE() nodepp::test_t()
+
+#define TEST_RUN( TEST ) TEST.run()
+
+/*---------------------------------------------------------------------------*/
+
+#define TEST_SKIP() return  0
+
+#define TEST_FAIL() return -1
+
+#define TEST_DONE() return  1 
 
 /*────────────────────────────────────────────────────────────────────────────*/
 
---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <nodepp/nodepp.h>
+#include <nodepp/test.h>
+
+using namespace nodepp;
+
+#include "test/console.cpp"
+
+onMain([](){
+
+    TEST::CONSOLE::TEST_RUNNER();
+
+})
