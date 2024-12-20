ebabd2770c26ba23f617e7807f7a51518a8aeddf
EDBC_REPO <109326461+EDBCREPO@users.noreply.github.com>
2024-12-20 18:46:03 -0400

    Update coroutine.h

--- include/nodepp/coroutine.h
+++ include/nodepp/coroutine.h
@@ -78,6 +78,7 @@ template< class T > T clamp( const T& val, const T& _min, const T& _max ){ retur
 
 /*────────────────────────────────────────────────────────────────────────────*/
 
+#define _JSON_(...) json::parse(_STRING_(__VA_ARGS__))
 #define _FUNC_  __PRETTY_FUNCTION__
 #define _STRING_(...) #__VA_ARGS__
 #define _NAME_  __FUNCTION__
